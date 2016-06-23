cab699a89feceb249dbc9e17efcd2d7fb918474d
Smit Dixit <dixits@purdue.edu>
2016-06-23 17:01:37 -0400

    add imgproc

--- ctImgKernels/mainsrc.cpp
+++ ctImgKernels/mainsrc.cpp
@@ -1,5 +1,10 @@
+//Uses CImg library for processing image files 
+//http://cimg.eu/
+//under the CeCILL license http://www.cecill.info/
+
 #include "kernel.cu"
-#include "jpeghandle.h"
+#include "CImg.h"
+using namespace cimg_library;
 
 int main()
 {
